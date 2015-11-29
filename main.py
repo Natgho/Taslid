2f1cad37b840ee2366500b5b2a0dc62926eb6a34
Sezer Bozkır <admin@sezerbozkir.com>
2015-11-29 20:27:17 +0200

    Ekleme ve düzenleme

--- main.py
+++ main.py
@@ -10,4 +10,31 @@
 # Sistemde bumblebee olup olmadığı kontrol edilecek.
 # Yoksa program sürdürülecek, varsa program sonlandırılacak
 
-#
+# Bumblebee repository'leri sisteme eklenecek
+
+# Kurulumlar gerçekleştirilecek
+
+#32 bit uyumluluk paketi yüklenecek
+
+# /etc/bumblebee/bumblebee.conf dosyası açılacak
+
+# Dosyanın içerisindeki Driver= ksımının devamına Nvidia eklenecek
+
+# KernelDriver=nvidia-current kısmı yüklenen nvidia sürümüyle değiştirilecek.
+
+# LibraryPath kısmının sonu editlenecek.
+
+# Default xorg modules... ile başlayan kısım edilenecek.
+
+# İşlemi garantiye almak için bumblebee servisi restarlanacak.
+
+# Çalışmasını kontrol etmek için normal glxgears başlatılıp FPS oranı incelenecek.
+
+# Optirun ile glxgears başlatılıp FPS oranı kontrol edilecek.
+
+# Programın doğru çalıştığına dair bilgi verilecek ve sistem bilgileri gösterilecek.
+
+# Kurulum sırasında faydalanılabilecek kaynaklar:
+# https://forum.linuxmint.net.tr/index.php?topic=78.0
+# http://enginsezen.com/blog/gnu-linux/31-bumblebee-kurulumu.html
+# http://ubuntulog.com/bumblebee-kurulumu.html
